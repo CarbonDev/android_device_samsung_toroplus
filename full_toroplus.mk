f09ca7386de4af51718e9eaeb3ca5f5065b90c2f
Liquid <rjbirnesser@gmail.com>
2013-01-21 21:55:40 -0500

    Liquify/Rebrand/Update Toroplus Device (Credit CM)

--- full_toroplus.mk
+++ full_toroplus.mk
@@ -1,4 +1,6 @@
-# Copyright (C) 2011 The Android Open Source Project
+#
+# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2012 The LiquidSmooth Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,32 +13,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#
-# This file is the build configuration for a full Android
-# build for toroplus hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and maguro, hence its name.
 #
 
 PRODUCT_PACKAGES := \
     GNexusParts
 
-# Get the long list of APNs
-# PRODUCT_COPY_FILES += device/samsung/toroplus/test-apns-conf_sprint.xml:system/etc/apns-conf.xml
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# This is where we'd set a backup provider if we had one
-#$(call inherit-product, device/sample/products/backup_overlay.mk)
 # Inherit from toroplus device
 $(call inherit-product, device/samsung/toroplus/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-#PRODUCT_NAME := full_toroplus
-#PRODUCT_DEVICE := toroplus
-#PRODUCT_BRAND := Android
-#PRODUCT_MODEL := Full AOSP on Toroplus
-#PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_NAME := liquid_toroplus
+PRODUCT_DEVICE := toroplus
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Full AOSP on Toroplus
+PRODUCT_RESTRICT_VENDOR_FILES := false
